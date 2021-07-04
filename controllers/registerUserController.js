29ca129edbff6822fbe49410ebd760f9785e46b6
bguilas <bryanjguilas@gmail.com>
2021-07-04 00:03:21 -0700

    register user route/auth created

--- controllers/registerUserController.js
+++ controllers/registerUserController.js
@@ -1,4 +1,56 @@
+const bcrypt = require('bcryptjs');
+const {validationResult} = require('express-validator');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const User = require('../models/UserSchema');
+
 module.exports.registerUser = async (req, res) => {
-	res.send('Register Route');
+	const errors = validationResult(req);
+
+	if (!errors.isEmpty()) {
+		return res.status(400).json({errors: errors.array()});
+	}
+
+	try {
+		const {name, email, password} = req.body;
+		let user = await User.findOne({email});
+
+		if (user) {
+			return res.status(400).json({errors: [{msg: 'User already exist'}]});
+		}
+
+		user = new User(
+			{
+				name,
+				email,
+				password
+			}
+		);
+
+		//Encrypt Password
+		const salt = await bcrypt.genSalt(10);
+		user.password = await bcrypt.hash(password, salt);
+
+		//Return Jsonwebtoken
+		const payload = {
+			user: {
+				id: user._id
+			}
+		};
+
+		jwt.sign(
+			payload,
+			config.get('jwtSecret'),
+			{expiresIn: 360000},
+			(err, token) => {
+				if (err) throw err;
+				res.json({token});
+			}
+		);
+		await user.save();
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send('Server Error');
+	}
 };
 
--- routes/api/registerUser.js
+++ routes/api/registerUser.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const {check} = require('express-validator');
 const registerUserController = require('../../controllers/registerUserController');
 
 router.route('/register')
-	.post(registerUserController.registerUser)
+	.post(
+		check('name', 'Name is required')
+			.not()
+			.isEmpty(),
+		check('email', 'Please include a valid email')
+			.isEmail(),
+		check('password', 'Please enter a password with 6 or more characters with at least one number')
+			.isLength({min: 6})
+			.matches(/\d/),
+		registerUserController.registerUser);
 
 module.exports = router;
