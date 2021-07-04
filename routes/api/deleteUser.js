43e2e7e585d4b0483f28093ff5b7f406efe63b3d
bguilas <bryanjguilas@gmail.com>
2021-07-04 14:01:06 -0700

    added /delete to delete route

--- routes/api/deleteUser.js
+++ routes/api/deleteUser.js
@@ -4,7 +4,7 @@ const deleteUserController = require('../../controllers/deleteUserController');
 const auth = require('../../middleware/auth');
 
 //api/user/delete/:user_id - Delete User
-router.route('/:user_id')
+router.route('/delete/:user_id')
 	.delete(auth, deleteUserController.deleteUser);
 
 module.exports = router;
